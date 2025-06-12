a15b13d96b1e1206c3262f75c52b76c511ba7e73
caiovilla <caiovila125@gmail.com>
2025-06-12 19:23:30 -0300

    feat: cadastro_produto

--- src/models/api.js
+++ src/models/api.js
@@ -1,56 +1,87 @@
-export class ApiRequest{
-    static url = "http://localhost:3000/api"
-    static token = localStorage.getItem("@token")
-    static headers = {
+export class ApiRequest {
+  static url = "http://localhost:3000/api";
+  static token = localStorage.getItem("@token");
+  static headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${this.token}`,
+  };
+  static async userRegister(data) {
+    const response = await fetch(`${this.url}/clientes/register`, {
+      method: "POST",
+      headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${this.token}`
-    }
-    static async userRegister(data){
-        const response = await fetch(`${this.url}/clientes/register`,{
-            method:"POST",
-            headers:{
-                "Content-Type": "application/json"
-            },
-            body:JSON.stringify(data)
-        })
-        const newUser = await response.json()
-        setTimeout(()=>{
-            window.location.replace("../../index.html")
+      },
+      body: JSON.stringify(data),
+    });
+    const newUser = await response.json();
+    setTimeout(() => {
+      window.location.replace("../../index.html");
+    }, 1000);
+    return newUser;
+  }
 
-        }, 1000)
-        return newUser
-    } 
+    static async createProducts(data){
+    const createProducts = await fetch(`${this.url}/produtos/create`, {
+        method:"POST",
+        headers: {
+        "Content-Type": "application/json",
+      },
+        body: JSON.stringify(data),
+    })
+    .then(res => res.json())
+    .then(res => {
+        setTimeout(() => {
+      window.location.replace("../../telaprincipal.html");
+    }, 1000);
+    return res
+    })
 
-    static async getProductClient(id){
-        const search = await fetch(`${this.url}/produtos/cliente/${id}`,{
-            method:"GET"
-        })
+    return createProducts
+  }
 
-        .then(res => res.json())
-        .then(res =>{
-            console.log(res)
-            return res
-        })
-        .catch (err => console.log(err))
-    
-        return search
-        
-    }
+  static async login(data) {
+    const user = await fetch(`${this.url}/clientes/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        localStorage.setItem("@token", res.token);
+        localStorage.setItem("@id", res.cliente.id);
+        setTimeout(() => window.location.replace("../../telaprincipal.html"));
+      })
+      .catch((err) => console.log(err));
+    return user;
+  }
 
-    static async deleteProduct(id){
-        const del = await fetch(`${this.url}/produtos/${id}`,{
-            method:"DELETE"
-        })
+  static async getProductClient(id) {
+    const search = await fetch(`${this.url}/produtos/cliente/${id}`, {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        return res;
+      })
+      .catch((err) => console.log(err));
 
-        .then(res=> res.json())
-        .then(res=>{
-            console.log(res)
-            return res
-        })
-        .catch (err=> console.log(err))
-        
-        window.location.reload()
-        return del
-    }
+    return search;
+  }
 
+  static async deleteProduct(id) {
+    const del = await fetch(`${this.url}/produtos/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        return res;
+      })
+      .catch((err) => console.log(err));
+
+    window.location.reload();
+    return del;
+  }
 }
